b8e277564f3503516b26b433ff413a2a002288ce
fahima1275 <lizafahima2003@gmail.com>
2025-01-13 23:48:59 +0600

    Add files via upload

---
+++ week 10 day 3/problem 1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    cin>>n>>q;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i=0; i<q; i++){
+        int key, l=0, r=n-1, mid;
+        cin>>key;
+        int found=0;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(key==a[mid]){
+                found=1;
+                break;
+            }
+            else if(key<a[mid]){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        if(found==1){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
